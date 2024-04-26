2ac82d303530d7e3568690726fd9294eab4f180d
Anton James Genabio <jamesgenabio31@gmail.com>
2024-04-26 21:51:16 +0800

    add student added

--- backend/adminBackend.php
+++ backend/adminBackend.php
@@ -22,6 +22,9 @@
     <title>Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
      <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body>
     
@@ -132,4 +135,30 @@ if(isset($_POST['submitEdit'])){
     }
 
 }
+
+if(isset($_POST['submitAdd'])){
+    $id_number = $_POST['id_number'];
+    $password = $_POST['password'];
+    $first_name = $_POST['first_name'];
+    $middle_name = $_POST['middle_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $course = $_POST['course'];
+    $year = $_POST['year'];
+
+  
+
+    $sql = "INSERT INTO `students` (`id_number`, `first_name`, `middle_name`, `last_name`, `email`,`course`,`year`,`password`)
+    VALUES('$id_number','$first_name','$middle_name','$last_name','$email','$course','$year','$password')";
+
+    if(mysqli_query($conn, $sql)){
+        echo '<script>alert("Register Successfull");</script>';
+        $conn->close();  
+    }
+    else{
+        echo '<script>alert("Duplicate Id Number");</script>';
+        $conn->close();  
+    }
+}
+
 ?>
