669e26710fa2c11a56518e524e82378fefc9c2b3
James Genabio <jamesgenabio31@gmail.com>
2024-04-26 19:06:40 +0800

    update register

--- backend/loginBackend.php
+++ backend/loginBackend.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+include 'connection.php';
+
 if(isset($_POST['submit'])){
     $id_number = $_POST['id_number'];
     $password = $_POST['password'];
@@ -15,5 +17,14 @@ if($_SESSION['adminId'] = 1){
     session_destroy();
 }
 
+if(isset($_POST['submitRegister'])){
+    $id_number = $_POST['id_number'];
+    $first_name = $_POST['first_name'];
+    $middle_name = $_POST['middle_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $course = $_POST['course'];
+    $year = $_POST['year'];
+}
 
 ?>
--- view/Register.php
+++ view/Register.php
@@ -1,10 +1,14 @@
+<?php
+    include '../backend/loginBackend.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Register</title>
-<!-- Bootstrap CSS -->
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 <style>
   body {
@@ -48,7 +52,7 @@
           Register
         </div>
         <div class="card-body">
-          <form>
+          <form action="Register.php" method="POST">
             <div class="mb-3">
               <label for="id_number" class="form-label">ID Number</label>
               <input type="text" class="form-control" id="id_number" name="id_number" required>
