6e67f4ccef3dcb22fc47d87aa414a140aade4b28
James Genabio <jamesgenabio31@gmail.com>
2024-04-26 16:52:26 +0800

    added sweet alert

--- backend/adminBackend.php
+++ backend/adminBackend.php
@@ -1,5 +1,6 @@
 <?php
-
+    session_start();
+    
 
 ?>
 <!DOCTYPE html>
@@ -9,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body>
     
@@ -39,3 +41,24 @@
     
 </body>
 </html>
+
+<?php
+    if($_SESSION['adminId'] == 1){
+         echo '<script>const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            }
+          });
+          Toast.fire({
+            icon: "success",
+            title: "Logged In!"
+          });</script>';
+
+    }
+?>
--- backend/loginBackend.php
+++ backend/loginBackend.php
@@ -1,11 +1,12 @@
 <?php
-
+session_start();
 if(isset($_POST['submit'])){
     $id_number = $_POST['id_number'];
     $password = $_POST['password'];
 
     if($id_number == 'admin' && $password == 'admin'){
         header('Location: ../view/Admin/Dashboard.php');
+        $_SESSION['adminId'] = 1;
     }
 }
 
