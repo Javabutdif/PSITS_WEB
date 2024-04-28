5726165f4e6a9b11d4d314ac2c003f60996275b0
Anton James Genabio <jamesgenabio31@gmail.com>
2024-04-28 15:20:19 +0800

    new database added

--- backend/adminBackend.php
+++ backend/adminBackend.php
@@ -107,8 +107,15 @@
             <a class="dropdown-item" href="../Admin/ReportMerch.php">Reports</a>
         </div>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="../Login.php">Logout</a>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Settings
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="../Admin/ViewMerch.php">Change Password</a>
+            <a class="dropdown-item" href="../Login.php">Logout</a>
+        
+        </div>
         </li>
       </ul>
     </div>
