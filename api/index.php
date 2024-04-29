abec77cda306c3d71e79b3e9cdb97941ebbd9575
Anton James Genabio <jamesgenabio31@gmail.com>
2024-04-29 20:38:22 +0800

    add

--- api/index.php
+++ api/index.php
@@ -1,16 +1,13 @@
-<?php 
-    header('Content-Type: application/json');
+<?php
+header('Content-Type: application/json');
 
-    if($_SERVER['REQUEST_METHOD'] === 'GET'){
-        $response = array(
-            'message'=> 'Hello, World!'
-        );
-        echo json_encode($respose);
-    }
-    else{
-        http_response_code(405);
-        echo json_encode(array('error'=>'Method Not Allowed'));
-    }
-
-
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $response = array(
+        'message' => 'Hello, World!'
+    );
+    echo json_encode($response);
+} else {
+    http_response_code(405);
+    echo json_encode(array('error' => 'Method Not Allowed'));
+}
 ?>
