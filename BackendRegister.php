7e534e8ae33ea67b12c99635fbadd72f4bf15ea0
Anton James Genabio <jamesgenabio31@gmail.com>
2024-05-01 17:54:22 +0800

    modify

--- BackendRegister.php
+++ BackendRegister.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
 include 'connection.php';
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -33,54 +31,32 @@ include 'connection.php';
 </body>
 </html>
 
-<?php 
+<?php
     
-if(isset($_POST['submit'])){
+
+if(isset($_POST['submitRegister'])){
     $id_number = $_POST['id_number'];
     $password = $_POST['password'];
+    $first_name = $_POST['first_name'];
+    $middle_name = $_POST['middle_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $course = $_POST['course'];
+    $year = $_POST['year'];
 
-    //Admin Login
-    $sqlAdmin = "SELECT * FROM `admin` WHERE id_number = '$id_number' AND password = '$password'";
-    $resultAdmin = mysqli_query($conn,$sqlAdmin);
-    $adminGet = mysqli_fetch_array($resultAdmin, MYSQLI_ASSOC);
-    //User Login
-    $sqlUser = "SELECT * FROM `students` WHERE id_number = '$id_number'";
-    $resultUser = mysqli_query($conn,$sqlUser);
-    $userGet = mysqli_fetch_array($resultUser, MYSQLI_ASSOC);
+    $hashPassword = password_hash($password,PASSWORD_DEFAULT );
 
-    if($adminGet['id_number'] != null){
-        ini_set('session.cookie_lifetime', 1800);
-        $_SESSION['adminId'] = $adminGet['id_number'];
-        $_SESSION['adminName'] = $adminGet['name'];
-        session_regenerate_id();
-        header('Location: AdminDashboard.php');
-        exit;
-    }
-    else if($userGet['id_number'] != null && $userGet['subscription'] == 'Approve' && password_verify($password,$userGet['password'])){
-        ini_set('session.cookie_lifetime', 1800);
-        $_SESSION['userId'] = $userGet['id_number'];
-        $_SESSION['userName'] = $userGet['first_name']." ".$userGet['middle_name'].". ".$userGet['last_name'];
-        session_regenerate_id();
-        header('Location: UserDashboard.php');
-        exit;
-    }
-    else if($userGet['id_number'] != null && $userGet['subscription'] == 'Pending' && password_verify($password,$userGet['password'])){
-        echo '<script>Swal.fire({
-					icon: "error",
-					title: "Oops...",
-					text: "You Need to Pay for the Subscription at the PSITS Office!",
-					
-				  });</script>';
+    $sql = "INSERT INTO `students` (`id_number`, `first_name`, `middle_name`, `last_name`, `email`,`course`,`year`,`password`, `status`,`subscription`)
+    VALUES('$id_number','$first_name','$middle_name','$last_name','$email','$course','$year','$hashPassword' , 'TRUE','Pending')";
+
+    if(mysqli_query($conn, $sql)){
+        echo '<script>alert("Register Successfull");</script>';
+        $conn->close();  
     }
     else{
-        echo '<script>Swal.fire({
-					icon: "error",
-					title: "Oops...",
-					text: "Incorret ID Number and Password!",
-					
-				  });</script>';
+        echo '<script>alert("Duplicate Id Number");</script>';
+        $conn->close();  
     }
 }
-
 
 ?>
--- connection.php
+++ connection.php
@@ -1,13 +1,12 @@
 <?php
-   error_reporting(0);
-$db_host = "localhost"; 
-$db_username = "root"; 
-$db_password = ""; 
-$db_name = "psits"; 
+error_reporting(0);
+$db_host = "sql304.infinityfree.com"; 
+$db_username = "if0_36435987"; 
+$db_password = "09083095890asd"; 
+$db_name = "if0_36435987_psits"; 
+$db_port = "3306"; 
 
-
-
-$conn = mysqli_connect($db_host, $db_username , $db_password, $db_name);
+$conn = mysqli_connect($db_host, $db_username , $db_password, $db_name, $db_port);
 
 date_default_timezone_set('Asia/Manila');
 
