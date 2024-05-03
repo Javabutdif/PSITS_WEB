5ff01e041ab440b647d98ce88d5e839b53d5fbee
Anton James Genabio <jamesgenabio31@gmail.com>
2024-05-03 20:10:38 +0800

    index inherit

--- index.php
+++ index.php
@@ -1,40 +1,19 @@
+<?php 
+    include 'indexInherit.php';
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Philippine Society of Information Technology Students</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
-     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    
    
      
 </head>
 <body>
         
-<nav class="navbar navbar-expand-lg" style="background-color:#074873">
-  <div class="container">
-    <img src="img/psits-logo.png" style="width:3rem; height:3rem;"/>
-    <a class="navbar-brand text-white" href="index.php"> Philippine Society of Information Technology Students - UC MAIN</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <a class="nav-link text-white" href="Login.php">Login</a>
-        </li>
-         <li class="nav-item">
-          <a class="nav-link text-white" href="Register.php">Register</a>
-        </li>
-       
-       
-      </ul>
-    </div>
-  </div>
-</nav>
 
 
 
